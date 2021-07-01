cd80df8cdb6368e67bbc7bab2c997c8d1094c5ba
vinu001 <vinayakbobade007@gmail.com>
2021-07-01 11:42:11 +0530

    Session1:Hello world and args taking on cmd

---
+++ session1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace session1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("First Program argument = " +args[0]);
+            Console.ReadLine();
+        }
+    }
+}
