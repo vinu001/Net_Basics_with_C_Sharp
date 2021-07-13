1365ea9450c499fd01ea63715b3586a8544f2fd9
vinu001 <vinayakbobade007@gmail.com>
2021-07-13 11:36:19 +0530

    Delegation Practice session by shravan --session7

---
+++ Session7/Calculate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session7
+{
+    public class Calculate
+    {
+        public delegate void AddSubDelegate(double num1, double num2);
+        public AddSubDelegate addSubDelegate;
+        public void Add (double num1, double num2)
+        {
+            Console.WriteLine("Addition of two umber is : " + (num1 + num2));
+        }
+        public void Sub(double num1, double num2)
+        {
+            Console.WriteLine("Substraction of two umber is : " + (num1 - num2));
+        }
+
+       
+    }
+}
---
+++ Session7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Session7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculate calculate = new Calculate();
+
+          //  calculate.addSubDelegate = calculate.Add;
+            calculate.addSubDelegate += calculate.Sub;
+
+            calculate.addSubDelegate(150, 50);
+        }
+    }
+}
